069bbc02ee2d5d22dbc339c698189a767662b939
antonpugachev <anton@wisedocs.com>
2026-03-04 16:13:09 -0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-// import { Task1 } from './tasks/Task1'
+import { Task1 } from './tasks/Task1'
 // import { Task2 } from './tasks/Task2'
 // import { Task3 } from './tasks/Task3'
-import { Task4 } from './tasks/Task4'
+// import { Task4 } from './tasks/Task4'
 
 function App() {
-  return <Task4 />
+  return <Task1 />
+  // return <Task2 />
+  // return <Task3 />
+  // return <Task4 />
 }
 
 export default App
