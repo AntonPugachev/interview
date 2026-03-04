64e206883d139770294f83ad8b0a687c84aaf3d0
antonpugachev <anton@wisedocs.com>
2026-03-04 09:30:55 -0500

    fix

--- src/tasks/Task2/Task2.tsx
+++ src/tasks/Task2/Task2.tsx
@@ -1,15 +1,15 @@
 export const Task2 = () => {
   return (
-    <div className=" w-full h-screen">
-      <header className="p-4  border-b ">
-        <h1 className="font-bold text-lg">Task: Aside + Main Layout</h1>
+    <div className="w-full h-screen">
+      <header className="p-4 border-b ">
+        <h1>Task: Aside + Main Layout</h1>
       </header>
       <div className="">
         <aside className={`bg-violet-200 w-75`}>Aside</aside>
-        <main className={``}>
+        <main className={'flex flex-col'}>
           <header className="p-4 bg-sky-200 h-16 ">Main Header</header>
-          <div className="p-4 bg-emerald-50 ">
-            <div className="h-screen">Main Content</div>
+          <div className="p-4 bg-emerald-50">
+            <div className="h-screen ">Main Content</div>
           </div>
           <footer className="p-4 bg-amber-200 h-16 ">Footer</footer>
         </main>
