82f63e4326e2f18004246415da5e0f99aa5f1cb4
antonpugachev <anton@wisedocs.com>
2026-03-04 09:21:22 -0500

    fix

--- src/tasks/Task2/Task2.tsx
+++ src/tasks/Task2/Task2.tsx
@@ -1,28 +1,13 @@
-import { useState } from 'react'
-
-import { BUTTON_BASE, NAV_ITEM_BASE } from './utils/constants'
-
 export const Task2 = () => {
-  const [isOpen, setIsOpen] = useState(true)
-
   return (
     <div className=" w-full h-screen">
       <header className="p-4  border-b ">
         <h1 className="font-bold text-lg">Task: Aside + Main Layout</h1>
       </header>
-
       <div className="">
-        <aside className={`bg-violet-200  ${isOpen ? 'w-75' : 'w-0'}`}>Aside</aside>
-
+        <aside className={`bg-violet-200 w-75`}>Aside</aside>
         <main className={``}>
-          <header className="p-4 bg-sky-200 h-16 ">
-            {!isOpen && (
-              <button className={`${BUTTON_BASE} mr-3`} onClick={() => setIsOpen(true)}>
-                Open
-              </button>
-            )}
-            Main Header
-          </header>
+          <header className="p-4 bg-sky-200 h-16 ">Main Header</header>
           <div className="p-4 bg-emerald-50 ">
             <div className="h-screen">Main Content</div>
           </div>
